56091770030326032fb9ec68e03df30daa78810a
Mauro E. Wernly <mauro.wernly@gmail.com>
2019-03-09 23:35:43 -0300

    Add logic to change weight, name and height

--- pokedex.js
+++ pokedex.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-    var $btnPokemonChange = $('.btn.sidebar-right:not(.btn-arrow)');
+    var $btnPokemonChange = $('.btn.sidebar-right:not(.btn-arrow)'),
+        pokemon = [];
+
+    $.getJSON('./pokedex.json', function(data) {         
+        pokemon = data;
+    });
 
     function getRandomPkmnSizing(min, max) {
         return Math.random() * (max - min) + min;
@@ -9,15 +14,18 @@ $(document).ready(function() {
         e.preventDefault();
 
         var $currentBtn = $(this),
-            currentPkmnId = parseInt($currentBtn.text()),
+            currentPkmnId = parseInt($currentBtn.text()) - 1,
             $pkmnHeightHolder = $('#pkm-height'),
             $pkmnWeightHolder = $('#pkm-weight'),
+            $pkmnNameHolder = $('#pkm-name'),
+            $pkmnJapaneseNameHolder = $('#japanese-name'),
             pkmnNewHeight = getRandomPkmnSizing(0.5, 14),
             pkmnNewWeight = getRandomPkmnSizing(0.1, 355);
 
-        alert(currentPkmnId);
-
         $pkmnHeightHolder.text(pkmnNewHeight.toFixed(2) + 'm');
         $pkmnWeightHolder.text(pkmnNewWeight.toFixed(2) + 'kg');
+
+        $pkmnNameHolder.text(pokemon[currentPkmnId].name.english);
+        $pkmnJapaneseNameHolder.text(pokemon[currentPkmnId].name.japanese)
     });
 });
